9dcc92af17175239260a7948d680e34aea3a1d69
SachinduNethmin <108050026+Sachindu-Nethmin@users.noreply.github.com>
2024-11-10 00:00:59 +0530

    red canvas
    Simple red canvas class

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
-class Renderer{
-    private canvas
+class Renderer
+{
+    private canvas: HTMLCanvasElement;
+    private g1: WebGL2RenderingContext;
+
+    constructor() {
+        this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+        this.g1 = this.canvas.getContext('webgl2') as WebGL2RenderingContext;
+    }
+    public render() {
+        this.g1.clearColor(1.0, 0.0, 0.0, 1.0);
+        this.g1.clear(this.g1.COLOR_BUFFER_BIT);
+    }
 }
 const renderer = new Renderer();
+renderer.render();
